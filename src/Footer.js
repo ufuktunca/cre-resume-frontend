1c5be6c69b21e1579a80c7ab3c4e3c97a7572ff3
ufuk barış tunca <ufukbaristunca@windowslive.com>
2022-06-11 11:45:35 +0300

    emoce footer on login and cv creation pages

--- src/Footer.js
+++ src/Footer.js
@@ -1,26 +1,30 @@
 import React from "react";
 
 export default function Footer() {
+  console.log(window.location.pathname);
   return (
     <footer>
-      <div className="footer-bottom-area footer-bg">
-        <div className="container">
-          <div className="footer-border">
-            <div className="row d-flex justify-content-between align-items-center">
-              <div className="col-xl-10 col-lg-10 ">
-                <div className="footer-copy-right">
-                  <p>
-                    This template is made by{" "}
-                    <a href="https://github.com/ufuktunca" target="_blank">
-                      Ufuk Barış Tunca
-                    </a>
-                  </p>
+      {window.location.pathname != "/login" &&
+        window.location.pathname != "create-cv" && (
+          <div className="footer-bottom-area footer-bg">
+            <div className="container">
+              <div className="footer-border">
+                <div className="row d-flex justify-content-between align-items-center">
+                  <div className="col-xl-10 col-lg-10 ">
+                    <div className="footer-copy-right">
+                      <p>
+                        This template is made by{" "}
+                        <a href="https://github.com/ufuktunca" target="_blank">
+                          Ufuk Barış Tunca
+                        </a>
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        )}
     </footer>
   );
 }
