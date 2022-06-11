65214c4d027611b13ab6629a3cab31fd5428814a
ufuk barış tunca <ufukbaristunca@windowslive.com>
2022-06-11 12:06:22 +0300

    foo

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-
           <Route
             path="/job-listing-employer"
             element={
